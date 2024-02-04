888a73455ad273bb08cc8ccfab8609f158ccd7fd
EbisaTeb <ebisateb@gmail.com>
2024-02-04 10:00:33 -0800

    i change user to body

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,23 @@
 import  mongoose from "mongoose";
-import { type } from "os";
 const userSchema =new mongoose.Schema({
     username:{
         type:String,
         require:true,
-        unique:true
+        unique:true,
     },
     email:{
         type:String,
         require:true,
-        unique:true
+        unique:true,
     },
     password:{
         type:String,
-        require:true
+        require:true,
     },
     avator:{
         type:String,
        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    }
+    },
 },{timestamps:true})
 const User=mongoose.model("User" ,userSchema)
 export default User;
