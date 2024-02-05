3b05c159261e52ac81c476c9fb62032cd27f98e6
EbisaTeb <ebisateb@gmail.com>
2024-02-05 00:28:42 -0800

    Fix Firebase configuration and add console log in OAuth component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -9,6 +9,7 @@ const handleGoogleClick= async()=>{
     const provider = new GoogleAuthProvider();
     const auth = getAuth(app);
     const result=await signInWithPopup(auth,provider)
+    console.log(result)
     const res=await fetch('/api/auth/google',{
       method:'POST',
       headers:{
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,22 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "eb-realstate.firebaseapp.com",
+//   projectId: "eb-realstate",
+//   storageBucket: "eb-realstate.appspot.com",
+//   messagingSenderId: "982153301778",
+//   appId: "1:982153301778:web:57345eb9b38bbbbdb3adcb"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +24,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "eb-realstate.firebaseapp.com",
-  projectId: "eb-realstate",
-  storageBucket: "eb-realstate.appspot.com",
-  messagingSenderId: "982153301778",
-  appId: "1:982153301778:web:57345eb9b38bbbbdb3adcb"
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY ,
+  authDomain: "ebs-realstate.firebaseapp.com",
+  projectId: "ebs-realstate",
+  storageBucket: "ebs-realstate.appspot.com",
+  messagingSenderId: "227166624360",
+  appId: "1:227166624360:web:249e424f16139843c279dd"
 };
 
 // Initialize Firebase
