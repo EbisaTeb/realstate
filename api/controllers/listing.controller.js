00a70c8de29785ed409b0ce4da18a6c12dec12a1
EbisaTeb <ebisateb@gmail.com>
2024-02-16 10:48:25 -0800

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export  const createListing = async (req, res,next) => {
     try {
@@ -7,4 +8,22 @@ export  const createListing = async (req, res,next) => {
     } catch (error) {
         next(error);
     }
+};
+
+export const deleteListing = async (req, res,next) => {
+ const listing=Listing.findById(req.params.id);    
+    if (!listing) {
+        return  next(errorHandler("Listing not found", 404) );
+
+    }
+   if (req.user._id !==Listing.userRef){
+         return next(errorHandler("You can omly delete your own  listings", 401));
+}
+   try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Listing deleted successfully" });
+    
+   } catch (error) {
+    next(error);
+   }
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -125,6 +125,22 @@ try {
   setShowListingsError(true);
 }
 }
+const handleListingDelete=async(listingId)=>{
+  try {
+    const res= await fetch(`/api/listing/delete/${listingId}`,{
+      method:'DELETE',
+    });
+    const data= await res.json();
+    if(data.success===false){
+      console.log(data.message);
+      return;
+    }
+    setShowListings((prev)=>prev.filter((listing)=>listing._id !==listingId));
+    
+  } catch (error) {
+    console.log(error.message);
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto' ><h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
     <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
@@ -170,7 +186,8 @@ try {
     </Link>
     <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}> <p>{listing.name}</p></Link>
     <div className=" flex flex-col items-center">
-      <button className='text-red-700 uppercase'  >Delete</button>
+      <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'  >Delete</button>
+      
       <button className='text-green-700 uppercase'  >edit</button>
     </div>
    </div>
