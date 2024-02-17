73b8f34e78e99c98850e37c5c38fb5d318f6ead6
EbisaTeb <ebisateb@gmail.com>
2024-02-17 12:07:14 -0800

    add image slider to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import {useParams} from 'react-router-dom';
-
+import {Swiper,SwiperSlide} from 'swiper/react';
+import SwiperCore from 'swiper'; 
+import { Navigation} from 'swiper/modules';
+import 'swiper/css/bundle';
 export default function Listing() {
+    SwiperCore.use([Navigation]);
     const [listing,setListing]=useState(null);
     const [loading,setLoading]=useState(true);
     const [error,setError]=useState(false);
@@ -10,7 +14,7 @@ export default function Listing() {
         const fetchListing = async () => {
             try {
                 setLoading(true);
-                const res = await fetch(`/api/user/listings/${params.listingId}`);
+                const res = await fetch(`/api/listing/get/${params.listingId}`);
                 const data = await res.json();
                 if (data.success === false) {
                     setError(true);
@@ -18,13 +22,32 @@ export default function Listing() {
                     return;
                 }
                 setListing(data);
+                setLoading(false);
+                setError(false);
             } catch (error) {
                 setError(true);
                 setLoading(false);
             }
         };
-        fetchListing()
+        fetchListing();
     },[params.listingId])
-  return <main>{ listing  && listing.name}</main>
+  return <main>{ loading  && <p className="text-center my-7 text-2xl">Loading</p>}
+  {error && <p className="text-center my-7 text-2xl">Something went wrong</p>}
+  {listing && !loading && !error &&
+  
+  ( <div>
+    <Swiper navigation>
+        {listing.imageUrls.map ((url) => (
+            <SwiperSlide key={url}>
+                <div 
+                 className="h-[550px]" style={{ background:`url(${url}) center no-repeat`,backgroundSize:'cover'}}>
+     
+                </div>
+               </SwiperSlide>
+                ))}        
+    </Swiper>
+  
+  </div> )}
+  </main>
   
 }
