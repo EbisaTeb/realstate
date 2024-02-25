03bfb1a527083afb5024428cef13574d083a8dd7
EbisaTeb <ebisateb@gmail.com>
2024-02-25 11:31:00 -0800

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
 import SignUp from './pages/SignUp'
 import UpdateListing from './pages/UpdateListing'
+import Search from './pages/Search'
 function App() {
   return (
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/listing/:listingId' element={<Listing />} />
+        <Route path='/search' element={<Search/>} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
