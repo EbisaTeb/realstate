8abc9a28cf2f98f6e21a68ef24a4a94224f6584a
EbisaTeb <ebisateb@gmail.com>
2024-02-28 11:55:32 -0800

    complete  about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div  className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800' >About Ebisa Estate </h1>
+      <p  className='mb-4 text-slate-700'>Ebisa Estate is a leading real estate agency that spacializes
+        in helping  client buy,sell,and rent properties inthe most desirable neighborthoods.
+        Our team of experienced agents is  dedicatited to  providing exceptional service and making the buying and selling process as smooth possible.
+      </p>
+      <br />
+      <p className='mb-4 text-slate-700'>Our mission is to help our clients achieve their  real estate 
+        goals by providing  expert advice ,persolized service and  a deep
+        understanding of the local market. 
+        whether you are looking to buy,sell,or rent a property, we are here to help you every step of the way.
+      </p>
+      <br />
+      <p className='mb-4 text-slate-700'> Our team of agents has a wealth of  experience and  
+        knowledge  in the real estate industry and we are committed
+        to providing the highest level of service to our clients.
+        We are believe  that buying or selling a property should be an exciting and
+         rewarding experience and we are dedicated to making that happen each and  every one of clients.
+      </p>
+    </div>
   )
 }
