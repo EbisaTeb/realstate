1771624aa19cb1ae70eccb16571239a62fb6c7f1
EbisaTeb <ebisateb@gmail.com>
2024-02-28 11:12:02 -0800

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -84,8 +84,9 @@ export default function Home() {
       </Swiper>
 
       {/* {Listing results for offer ,sale and rent } */}
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
-      {
+      <div className=" max-w-6xl  md:container md:mx-auto p-3 flex flex-col gap-8 my-10">
+     
+    {
            offerListings && offerListings.length>0 &&(
             <div className="">
               <div className="my-3">
@@ -100,7 +101,8 @@ export default function Home() {
             </div>
             </div>
             )}
-            {
+            
+       {
               rentListings && rentListings.length>0 &&(
                <div className="">
                  <div className="my-3">
@@ -115,7 +117,8 @@ export default function Home() {
                </div>
                </div>
                )}
-               {
+              
+         {
                 saleListings && saleListings.length>0 &&(
                  <div className="">
                    <div className="my-3">
